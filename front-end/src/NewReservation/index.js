3a64cdd1a4beb1aedb98084c8d71a0408d815452
Pablo Munoz-Evers <117239858+pamunev@users.noreply.github.com>
2023-09-01 15:59:57 -0500

    Internal server error when submitting reservation form

--- front-end/src/NewReservation/index.js
+++ front-end/src/NewReservation/index.js
@@ -52,7 +52,7 @@ function NewReservation() {
         event.preventDefault()
         const abortController = new AbortController()
         try {
-            await createReservation(formData, abortController.signal),
+            await createReservation(formData, abortController.signal)
             history.push(`/dashboard?date=${formData.reservation_date}`)
         } catch(error) {
             setReservationError(error)
