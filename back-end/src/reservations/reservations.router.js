7105b21705e7fef8314aef26d5018e092903bbfd
Pablo Munoz-Evers <117239858+pamunev@users.noreply.github.com>
2023-09-01 16:45:56 -0500

    no reservations/new

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -9,6 +9,6 @@ const controller = require("./reservations.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed")
 
 router.route("/").get(controller.list).post(controller.create).all(methodNotAllowed);
-router.route("/new").get(controller.list).post(controller.create).all(methodNotAllowed)
+
 
 module.exports = router;
